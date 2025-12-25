0aa1eb2d9e52f15de806f61dfad06fdce475f0c6
fredericgahounzo4-oss <fredericgahounzo4@gmail.com>
2025-12-25 22:28:01 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,416 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>FREDERIC le Dévéloppeur</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="a.css">
+
+</head>
+
+<body class="index-page">
+
+  <header id="header" class="header dark-background d-flex flex-column">
+    <i class="header-toggle d-xl-none bi bi-list"></i>
+
+    <div class="profile-img">
+      <!-- <img src="assets/img/my-profile-img.jpg" alt="" class="img-fluid rounded-circle"> -->
+      <img src="mon image.jpeg" alt="" class="img-fluid rounded-circle">
+    </div>
+
+    <a href="index.html" class="logo d-flex align-items-center justify-content-center">
+     <!--  <img src="assets/img/logo.png" alt=""> -->
+      <h1 class="sitename">Bienvenue sur mon site</h1>
+    </a>
+
+  <div class="social-links text-center">
+    <a href="https://x.com/FREDERICGA18876?s=09" class="twitter"><i class="bi bi-twitter-x"></i></a>
+    <a href="https://www.facebook.com/koffi.frederic.4356?locale=fr_FR" class="facebook"><i class="bi bi-facebook"></i></a>
+    <a href="https://www.instagram.com/gahounzokoffifrederic/" class="instagram"><i class="bi bi-instagram"></i></a>
+    <a href="https://web.telegram.org/k/" class="google-plus"><i class="bi bi-skype"></i></a>
+    <a href="https://www.linkedin.com/in/frederic-gahounzo-541650350/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+    </div> 
+
+    <nav id="navmenu" class="navmenu">
+      <ul>
+        <li><a href="#hero" class="active"><i class="bi bi-house navicon"></i>Home</a></li>
+        <li><a href="#about"><i class="bi bi-person navicon"></i> presentation</a></li>
+        <li><a href="#resume"><i class="bi bi-file-earmark-text navicon"></i> resume</a></li>
+        <li><a href="#services"><i class="bi bi-hdd-stack navicon"></i> Services</a></li>
+        <li><a href="#contact"><i class="bi bi-envelope navicon"></i> Contact</a></li>
+      </ul>
+    </nav>
+
+  </header>
+
+  <main class="main">
+
+    <section id="hero" class="hero section dark-background">
+
+      <!-- <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in" class=""> -->
+      <img src="web.jpg" alt="" data-aos="fade-in" class="">
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+        <h2>frederic</h2>
+        <p><span class="typed" data-typed-items="KOFFI,FREDERIC,GAHOUNZO"> Designer </span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
+      </div>
+
+    </section>
+
+    <section id="about" class="about section">
+
+      <div class="container section-title" data-aos="fade-up">
+        <h2>À propos de moi</h2>
+        <p>Je suis un développeur web passionné, prêt à créer votre site web sur mesure.</p>
+      </div>
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4 justify-content-center">
+          <div class="col-lg-4">
+            <!-- <img src="assets/img/my-profile-img.jpg" class="img-fluid" alt=""> -->
+            <img src="mon image.jpeg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 content">
+           
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <!-- <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>4 Février 2005</span></li> -->
+                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>frederic-web.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+228 79703458</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Lomé, TOGO</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <!-- <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>20</span></li> -->
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>formation professionnelle </span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>fredericgahounzo4@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                </ul>
+              </div>
+            </div>
+            <p class="py-3">
+              Laisse-moi le temps de me présenter: Je m'appelle Frédéric Gahounzo, développeur web passionné et créatif, spécialisé dans la conception de sites dynamiques et interactifs. Fort de mon expertise, je mets mon savoir-faire au service de mes clients pour créer des expériences web uniques et sur mesure.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
+    <section id="skills" class="skills section light-background">
+
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Mes Compétences</h2>
+        <p>
+          <strong>Langages :</strong> HTML, CSS, JavaScript, PHP.<br>
+          <!-- <strong>Frameworks :</strong> React.js, Vue.js, Bootstrap, Tailwind CSS.<br> --> 
+          <strong>Back-end :</strong> <!-- Node.js, Express.js --> MySQL, MongoDB.<br>
+       <!--   <strong>Outils :</strong> Git, GitHub, Webpack.<br>
+          <strong>Autres :</strong> SEO, Responsive Design, Optimisation des performances. -->
+        </p>
+      </div>
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row skills-content skills-animation">
+
+          <div class="col-lg-6">
+
+            <div class="progress">
+              <span class="skill"><span>HTML</span> <i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill"><span>CSS</span> <i class="val">95%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill"><span>JavaScript</span> <i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6">
+
+            <div class="progress">
+              <span class="skill"><span>PHP</span> <i class="val">70%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+           <!--  <div class="progress">
+              <span class="skill"><span>WordPress/CMS</span> <i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill"><span>Photoshop</span> <i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div> -->
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+     <section id="resume" class="resume section">
+
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Resume</h2>
+        <p>Développeur web junior diplômé, passionné par la création de sites web modernes, interactifs et responsives. Maîtrise des langages de base tels que HTML5, CSS3, ainsi que des technologies back-end comme PHP et la gestion de bases de données MySQL. Capable de concevoir des sites web dynamiques, de créer des interfaces utilisateur attrayantes, et d'assurer la connexion avec des bases de données pour une expérience complète.</p>
+      </div>
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Education</h3>
+
+            <div class="resume-item">
+              <h4>Education</h4>
+              <h5>2023</h5>
+              <p><em>Baccalauréat Scientifique – Série D Lycée de Vogan 1, Togo</em></p>
+              <p>C'est un accomplissement remarquable qui ouvre la voie à de belles perspectives d'avenir dans les sciences et la technologie.</p>
+            </div>
+
+            <div class="resume-item">
+              <h4>Formation</h4>
+              <h5>2024 - 2025</h5>
+              <p><em>Diplôme professionnel en Développement Web,HTML, CSS, PHP (Back-end), MySQL, au centre de formation Tchep Consulting Service – Lomé, Togo 2025</em></p>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+            <h3 class="resume-title">Expérience professionnelle</h3>
+            <div class="resume-item">
+              <h4>Développeur web – Projets personnels et académiques</h4>
+              <h5>2024 - 2025 </h5>
+              <p><em>Lomé, Togo</em></p>
+              <ul>
+                <li>Création d’un site personnel de présentation (HTML/CSS/JavaScript), incluant une page d’accueil, une section contact, et un formulaire fonctionnel. </li>
+                <li>Réalisation d’un site e-commerce dynamique pour la vente de sacs, chaussures et accessoires, avec tri des produits par catégorie. </li>
+                <li>Utilisation de PHPMailer pour l’envoi d’e-mails via formulaire de contact. </li>
+              </ul>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+    <section id="services" class="services section">
+
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Mes Services</h2>
+      </div>
+
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon flex-shrink-0"><i class="bi bi-briefcase"></i></div>
+            <div>
+              <h4 class="title"><!-- <a href="service-details.html" class="stretched-link" -->🌐 Création de sites web<!-- </a> --></h4>
+              <p class="description">Sites modernes, responsives et dynamiques avec HTML, CSS, JavaScript.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon flex-shrink-0"><i class="bi bi-card-checklist"></i></div>
+            <div>
+              <h4 class="title"><!-- <a href="service-details.html" class="stretched-link" -->🛒 Boutique en ligne<!-- </a> --></h4>
+              <p class="description">Sites e-commerce avec panier, paiement et gestion de produits comme sacs, chaussures, accessoires, etc.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon flex-shrink-0"><i class="bi bi-bar-chart"></i></div>
+            <div>
+              <h4 class="title"><!-- <a href="service-details.html" class="stretched-link" -->🎨 Design personnalisé<!-- </a> --></h4>
+              <p class="description">Designs attractifs avec animations, effets de survol, et présentation soignée..</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="400">
+            <div class="icon flex-shrink-0"><i class="bi bi-binoculars"></i></div>
+            <div>
+              <h4 class="title"><!-- <a href="service-details.html" class="stretched-link" -->🧑‍ Présentation interactive<!-- </a> --></h4>
+              <p class="description">Pages de présentation professionnelle avec biographie, CV, photos et autres.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon flex-shrink-0"><i class="bi bi-brightness-high"></i></div>
+            <div>
+              <h4 class="title"><!-- <a href="service-details.html" class="stretched-link" -->🔐 Systèmes de connexion<!-- </a> --></h4>
+              <p class="description">Fonctionnalités d’inscription, de connexion sécurisée et gestion des utilisateurs.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section id="contact" class="contact section">
+
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Contact</h2>
+        <p>Vous pouvez nous joindre par téléphone.</p>
+
+      </div>
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-5">
+
+            <div class="info-wrap">
+              <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="200">
+                <i class="bi bi-geo-alt flex-shrink-0"></i>
+                <div>
+                  <h3>Adresse</h3>
+                  <p>TOGO, LOME, Agoe Assiyéyé </p>
+                </div>
+              </div>
+
+              <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
+                <i class="bi bi-telephone flex-shrink-0"></i>
+                <div>
+                  <h3>Contactez-moi</h3>
+                  <p>+228 79703458/91470497</p>
+                </div>
+              </div>
+
+              <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
+                <i class="bi bi-envelope flex-shrink-0"></i>
+                <div>
+                  <h3>Email</h3>
+                  <p>fredericgahounzo4@gmail.com</p>
+                </div>
+              </div>
+
+            <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" style="border:0; width: 100%; height: 270px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+
+			      <iframe src="https://www.google.com/maps?q=March%C3%A9+Ago%C3%A8+Assiy%C3%A9y%C3%A9, +Lom%C3%A9,+Togo&hl=fr&z=16&output=embed" frameborder="0" style="border:0; width: 100%;height: 270px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+          </div>
+          </div>
+
+          <div class="col-lg-7">
+            <form action="forms/contact.php" method="post" class="" data-aos="fade-up" data-aos-delay="200">
+              <div class="row gy-4">
+
+                <div class="col-md-6">
+                  <label for="name-field" class="pb-2">Your Name</label>
+                  <input type="name" name="name" id="name-field" class="form-control" required="">
+                </div>
+
+                <div class="col-md-6">
+                  <label for="email-field" class="pb-2">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email-field" required="">
+                </div>
+
+                <div class="col-md-12">
+                  <label for="subject-field" class="pb-2">Subject</label>
+                  <input type="Subject" class="form-control" name="subject" id="subject-field" required="">
+                </div>
+
+                <div class="col-md-12">
+                  <label for="message-field" class="pb-2">Message</label>
+                  <textarea class="form-control" name="message" rows="10" id="message-field" required=""></textarea>
+                </div>
+
+                <div class="col-md-12 text-center">
+                  <button type="submit" name="submit">Send Message</button>
+                </div> 
+
+              </div>
+            </form>
+          </div>
+
+        </div>
+
+      </div> 
+
+    </section>
+
+  </main>
+
+  <footer id="footer" class="footer position-relative light-background">
+
+    <div class="container">
+      <div class="copyright text-center ">
+        <p><span>&copy;</span> <strong class="px-1 sitename"></strong> <span>2025 Mon Site Web. Tous droits réservés.</span></p>
+      </div>
+    </div>
+
+  </footer>
+
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <script src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>
